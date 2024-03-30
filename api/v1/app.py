a5a520d5bcd7c82db1a42c2c84d94057d70db2a9
Isma3l-19 <133232846+Isma3l-19@users.noreply.github.com>
2024-03-30 15:24:25 +0000

    Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Creating a Flask Application"""
+
+import os
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from flask_cors import CORS
+
+
+#Creating a flask application instance
+app = Flask(__name__)
+
+#Registering the blueprint app_views
+app.register_blueprint(app_views)
+
+#Initializing CORS with the app instance
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+#Declaring a method to handle teardown
+@app.teardown_appcontext
+def teardown(exception):
+    """Closes the current SQLAlchemy session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles the 404 not found error"""
+    return({'error': 'Not found'}), 404
+
+if __name__ == '__main__':
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
+    #getenv returns a string and port is an int
+    #threaded is set to true so it can serve multiple request at once
---
+++ api/v1/views/index.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Returns a JSON format response"""
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+@app_views.route('/status')
+def status_check():
+    """returns the  status code"""
+    return jsonify({"status": "OK"})
+
+@app_views.route('/status', methods=['GET'])
+def object_stats():
+    """Retrieves the number of each objecct by type"""
+    objects = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User'),
+    }
+    return jsonify(objects)
