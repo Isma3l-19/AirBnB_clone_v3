f49c9383c601a54fdca34af9a07a59ef60e6c2dc
LINNETWAHOME02 <98949954+LINNETWAHOME02@users.noreply.github.com>
2024-03-31 19:21:32 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,40 +1,45 @@
 #!/usr/bin/python3
-"""Creating a Flask Application"""
+"""
+app
+"""
 
-import os
-from flask import Flask
-from models import storage
-from api.v1.views import app_views
+from flask import Flask, jsonify
 from flask_cors import CORS
+from os import getenv
+
+from api.v1.views import app_views
+from models import storage
 
 
-#Creating a flask application instance
 app = Flask(__name__)
 
-#Registering the blueprint app_views
-app.register_blueprint(app_views)
-
-#Initializing CORS with the app instance
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+app.register_blueprint(app_views)
 
-#Declaring a method to handle teardown
+
 @app.teardown_appcontext
 def teardown(exception):
-    """Closes the current SQLAlchemy session"""
+    """
+    teardown function
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """Handles the 404 not found error"""
-    return({'error': 'Not found'}), 404
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
 
+    resp = jsonify(data)
+    resp.status_code = 404
 
-if __name__ == '__main__':
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
-    #getenv returns a string and port is an int
-    #threaded is set to true so it can serve multiple request at once
-    
+    return(resp)
+
+if __name__ == "__main__":
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
